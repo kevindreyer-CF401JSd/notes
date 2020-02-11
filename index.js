206bf114413ec0b80b7505bbc9c0e13a2901761a
AstroKD <kdreyerinseattle@gmail.com>
2020-02-10 18:15:39 -0800

    running server, trying to get help

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+'use strict';
+
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
+
+const options = new Input();
+options.valid() ? Notes.fetch(options) : help();
+
+function help() {
+    console.log(`
+    api USAGE: api -a <text note>
+
+     -a - String of text
+    `);
+    process.exit();
+}
